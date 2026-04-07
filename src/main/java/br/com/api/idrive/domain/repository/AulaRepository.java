d218d311c11ade1db3e495f69f81df87a878689d
Maylson276 <162899551+MaylsonS@users.noreply.github.com>
2026-04-07 17:28:47 -0300

    Ajustando o aulaRepository para se buscar as aulas do aluno, e o instrutor.

--- src/main/java/br/com/api/idrive/domain/repository/AulaRepository.java
+++ src/main/java/br/com/api/idrive/domain/repository/AulaRepository.java
@@ -4,6 +4,13 @@ import br.com.api.idrive.domain.model.Aula;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
-public interface AulaRepository extends JpaRepository<Aula,Long> {
+public interface AulaRepository extends JpaRepository<Aula, UUID> {
+
+    List<Aula> findByAlunoId(UUID alunoId);
+
+    List<Aula> findByInstrutorId(UUID instrutorId);
 }
