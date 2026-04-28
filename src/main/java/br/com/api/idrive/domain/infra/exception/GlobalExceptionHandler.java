00c190b3461d552b66060c6e942cf7d8d67cc075
gabrielsdev <gabrielsndev@gmail.com>
2026-04-28 13:20:21 -0300

    criando tratamento de exceções

---
+++ src/main/java/br/com/api/idrive/domain/infra/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package br.com.api.idrive.domain.infra.exception;
+
+import br.com.api.idrive.domain.dto.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // AQUI COLOCA EXCEÇÕES CUSTOMIZADAS
+
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Erro Interno do Servidor",
+                "Ocorreu um erro inesperado no sistema.",
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+}
